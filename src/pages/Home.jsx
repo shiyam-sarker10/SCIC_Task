f0e0f38f3f3adad9a07eb4c4546760dfbc6fdbf0
shiyam-sarker10 <kaneki438500929@gmail.com>
2024-01-15 19:26:23 +0600

    added testimonial section with responsive carousal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,39 @@ import React from 'react';
 import Banner from '../component/Banner/Banner';
 import allTask from '../assets/allTask.png'
 import PWGB from '../component/PWGB/PWGB';
+import Testimonial from '../component/Testimonial/Testimonial';
 
 const Home = () => {
     return (
       <div>
         <Banner></Banner>
-        <div className='my-20'>
-            <h1 className=' text-3xl text-gray-900 lg:text-4xl text-center font-semibold'>Manage your Daily Todo Tasks</h1>
-            <p className='text-gray-500 py-6 text-sm md:text-base w-[40%] mx-auto text-center'>With  the help of Taskiee's clean user interface you  can easily manage your daily task routine with just a simple drag and drop </p>
-          <img className='w-[80%] rounded-2xl shadow-lg mx-auto my-6'  src={allTask} alt="" />
+        <div className="my-20">
+          <h1 className=" text-3xl text-gray-900 lg:text-4xl text-center font-semibold">
+            Manage your Daily Todo Tasks
+          </h1>
+          <p className="text-gray-500 py-6 text-sm md:text-base w-[40%] mx-auto text-center">
+            With the help of Taskiee's clean user interface you can easily
+            manage your daily task routine with just a simple drag and drop{" "}
+          </p>
+          <img
+            className="w-[80%] rounded-2xl shadow-lg mx-auto my-6"
+            src={allTask}
+            alt=""
+          />
         </div>
         <PWGB></PWGB>
-        
+        <div>
+          <h1 className=" text-3xl text-gray-900 lg:text-4xl text-center font-semibold">
+            Peoples Testimonials
+          </h1>
+          <p className="text-gray-500 py-6 text-sm md:text-base w-[50%] mx-auto text-center">
+            Our users, spanning various professional backgrounds, have shared
+            impactful statements about the benefits they've experienced through
+            Taskiee in their respective fields.
+          </p>
+        </div>
+
+        <Testimonial />
       </div>
     );
 };
